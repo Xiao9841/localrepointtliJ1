aee31e3a7010409fbcff91d34f6294d941287fe9
Xiao9841 <alishasunuwar1111@gmail.com>
2024-06-10 12:13:09 -0400

    some changes

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static int getNumDivisbleOrder(int lastOrderNumber){
+        int numDivisibleOrders = 0;
+
+        for(int i = 1; i <= lastOrderNumber; i++){
+            if(i % 9 == 0){
+                numDivisibleOrders++;
+            }
+//        int i = 1;
+//        while(i <= lastOrderNumber){
+//            if( i % 9 == 0){
+//                numDivisibleOrders++;
+//            }
+//            i++;
+        }
+
+        return numDivisibleOrders;
+    }
+    /* public is modifier, static -> method belongs to class, String -> accept command line argument any argument provided
+    on the command line are passed as string in this array. if no argument are provided, the array will be empty(but not "null")
+     */
+    public static void main(String [] args){
+
+        System.out.println(getNumDivisbleOrder(18));
+    }
+
+
+}
